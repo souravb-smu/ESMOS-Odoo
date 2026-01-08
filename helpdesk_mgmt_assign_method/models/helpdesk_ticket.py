f4d9b594871647fc9b9463d362eceb3cc9dec7c8
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2026-01-08 20:20:55 -0300

    [MIG] helpdesk_mgmt_assign_method: Migration to 17.0

--- helpdesk_mgmt_assign_method/models/helpdesk_ticket.py
+++ helpdesk_mgmt_assign_method/models/helpdesk_ticket.py
@@ -9,16 +9,16 @@ class HelpdeskTicket(models.Model):
 
     @api.model
     def default_get(self, fields_list):
-        """Assign user based on team when creating a new ticket."""
         res = super().default_get(fields_list)
-        team_id = res.get("team_id")
-        if (
-            (not fields_list or "user_id" in fields_list)
-            and "user_id" not in res
-            and team_id
-        ):
+        team_id = res.get("team_id") or self.env.context.get("default_team_id")
+        if not team_id:
+            return res
+        if "user_id" in res and "default_user_id" not in self.env.context:
+            res.pop("user_id")
+        if (not fields_list or "user_id" in fields_list) and "user_id" not in res:
             team = self.env["helpdesk.ticket.team"].browse(team_id)
-            res["user_id"] = team.get_new_user().id
+            if team.assign_method != "manual":
+                res["user_id"] = team.get_new_user().id
         return res
 
     @api.onchange("team_id")
@@ -31,7 +31,16 @@ class HelpdeskTicket(models.Model):
     def create(self, vals):
         """Assign user based on team on creation if not provided."""
         team_id = vals.get("team_id")
-        if team_id and not vals.get("user_id"):
+        if team_id:
             team = self.env["helpdesk.ticket.team"].browse(team_id)
-            vals["user_id"] = team.get_new_user().id
-        return super(HelpdeskTicket, self).create(vals)
+            user_id = vals.get("user_id")
+            if user_id:
+                if user_id not in team.user_ids.ids:
+                    if team.assign_method == "manual":
+                        vals["user_id"] = False
+                    else:
+                        vals["user_id"] = team.get_new_user().id
+            else:
+                if team.assign_method != "manual":
+                    vals["user_id"] = team.get_new_user().id
+        return super().create(vals)
